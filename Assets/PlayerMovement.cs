5a8f378fa57f33512a9d2a35eb4e1b71981876a5
Ben Harris <benjharris@hotmail.co.uk>
2020-01-30 22:49:19 +0000

    first commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+        
+    // Update is called once per frame
+    void Update()
+    {
+        // Input
+
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        rb.velocity = new Vector2(movement.x, movement.y);
+    }
+}
