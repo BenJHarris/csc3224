1a813561b6bfa5504878c174d36f064daf0ce233
Ben Harris <benjharris@hotmail.co.uk>
2020-02-08 23:23:57 +0000

    more light tomfoolery

---
+++ Assets/SwitchRotationWithPlayerDirection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchRotationWithPlayerDirection : MonoBehaviour
+{
+
+    public PlayerMovement playerMovement;
+
+
+    void Update()
+    {
+        Vector2 direction = playerMovement.direction;
+
+        if (direction == Vector2.up)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+        else if (direction == Vector2.left)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        else if (direction == Vector2.down)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+        else if (direction == Vector2.right)
+        {
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 270));
+        }
+
+    }
+}
