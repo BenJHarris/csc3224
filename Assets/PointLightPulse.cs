54f32891e9c592f2554c018f7d17c91fb07a7411
Ben Harris <benjharris@hotmail.co.uk>
2020-02-08 18:13:17 +0000

    lights

---
+++ Assets/PointLightPulse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class PointLightPulse : MonoBehaviour
+{
+
+    public Light2D pointLight;
+
+    public void Update()
+    {
+        pointLight.pointLightOuterRadius = 1 + 3  * Mathf.Abs(Mathf.Sin(Time.time));
+    }
+}
